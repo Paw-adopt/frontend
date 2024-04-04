e498ab85bb8c2c73e56f8085c99721b1993782c0
m3llad0 <a01655451@tec.mx>
2024-04-04 13:38:39 -0600

    created test for contact page

---
+++ src/test/pages/contacto/page.test.tsx
@@ -0,0 +1,37 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import ContactPage from '@/app/contacto/page';
+
+describe('ContactPage', () => {
+  beforeEach(() => {
+    render(<ContactPage />);
+  });
+
+  it('should render the form fields', () => {
+    const firstNameInput = screen.getByRole('textbox', { name: /nombre/i });
+    expect(firstNameInput).toBeInTheDocument();
+
+    const lastNameInput = screen.getByRole('textbox', { name: /apellido/i });
+    expect(lastNameInput).toBeInTheDocument();
+
+    const emailInput = screen.getByRole('textbox', { name: /correo/i });
+    expect(emailInput).toBeInTheDocument();
+
+    const messageInput = screen.getByRole('textbox', { name: /mensaje/i });
+    expect(messageInput).toBeInTheDocument();
+  });
+
+  it('should render the submit button', () => {
+    const submitButton = screen.getByRole('button', { name: /hablemos/i });
+    expect(submitButton).toBeInTheDocument();
+    expect(submitButton).toBeDisabled();
+  });
+
+  it('should enable the submit button when the switch is checked', () => {
+    const switchButton = screen.getByRole('checkbox', { name: /agree to policies/i });
+    fireEvent.click(switchButton);
+
+    const submitButton = screen.getByRole('button', { name: /hablemos/i });
+    expect(submitButton).not.toBeDisabled();
+  });
+});
