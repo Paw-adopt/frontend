e281a6fffa52cb1c6112fa8c274573b846d2d903
m3llad0 <a01655451@tec.mx>
2024-04-08 22:00:26 -0600

    updated icons: changed icons colors

--- src/app/publica-con-nosotros/page.tsx
+++ src/app/publica-con-nosotros/page.tsx
@@ -47,7 +47,7 @@ export default function PublishWithUs() {
                       {features.map((feature) => (
                       <article key={feature.name} className="relative pl-9">
                           <dt className="inline font-semibold text-gray-900">
-                          <feature.icon className="absolute left-1 top-1 h-5 w-5 text-indigo-600" aria-hidden="true" />
+                          <feature.icon className="absolute left-1 top-1 h-5 w-5 text-[#FFB350]" aria-hidden="true" />
                           {feature.name}
                           </dt>{' '}
                           <dd className="inline">{feature.description}</dd>
