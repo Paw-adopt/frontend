9b460b35dc665111de2377309097903c19faebda
Diego <70420658+m3llad0@users.noreply.github.com>
2024-04-10 17:36:41 -0600

    Update src/components/footer.tsx
    Dynamic date to keep copyright year updated
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer(){
     <footer className="bg-white rounded-lg shadow">
         <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span data-testid = "copyright" className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © 2023 <a className="hover:underline">Paw adopt™</a>. All Rights Reserved.
+            © {new Date().getFullYear()} <a className="hover:underline">Paw adopt™</a>. All Rights Reserved.
         </span>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0 space-x-5">
             <li>
